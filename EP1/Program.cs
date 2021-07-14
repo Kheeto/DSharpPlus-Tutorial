5da84e48bb1a28a47c0eacc2185f9af69f9b9db0
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-14 17:52:03 +0200

    Create Program.cs

---
+++ EP1/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace DSharpPlusTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.EseguiBot().GetAwaiter().GetResult();
+        }
+    }
+
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+
+        public async Task EseguiBot()
+        {
+            DiscordConfiguration configBot = new DiscordConfiguration
+            {
+                Token = "Njg3MDQ1MTgwMTc1ODEwNjIx.XmgCrw.jNcK1DV-e7fwoqilRxQIwWpqIBE",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+            };
+
+            Client = new DiscordClient(configBot);
+
+            Client.Ready += OnReady;
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnReady(object sender, ReadyEventArgs ev)
+        {
+            Console.WriteLine("Bot Avviato");
+            return Task.CompletedTask;
+        }
+    }
+}
