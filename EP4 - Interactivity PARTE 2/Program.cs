61bdded41a2c126b1871adae2b97837be8747b4b
Kheeto <70845116+Kheeto@users.noreply.github.com>
2021-07-16 19:19:14 +0200

    Update Program.cs

--- EP4 - Interactivity PARTE 2/Program.cs
+++ EP4 - Interactivity PARTE 2/Program.cs
@@ -30,7 +30,7 @@ namespace DSharpPlusTutorial
         {
             DiscordConfiguration configBot = new DiscordConfiguration
             {
-                Token = "INSERIRE IL TOKEN QUI",
+                Token = "INSERIRE IL TOKEN DEL BOT QUI",
                 TokenType = TokenType.Bot,
                 AutoReconnect = true,
                 MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
